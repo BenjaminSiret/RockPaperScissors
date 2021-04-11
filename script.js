2ebbbdf5283f47f872df916d5f29cc309bd72cd6
Benjamin Siret <benjaminsiret@outlook.fr>
2021-04-11 13:06:03 +0200

    add function game in script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let actions = ['Rock', 'Paper', 'Scissors'];
 let computerPlay = () => actions[Math.floor(Math.random() * actions.length)];
 
-computerSelection = computerPlay();
+function playRound(playerSelection, computerSelection) {
+  computerSelection = computerPlay().toLowerCase();
+  playerSelection = window.prompt('Choose Rock, Paper or Scissors').toLowerCase();
+  if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+      (playerSelection == 'paper' && computerSelection == 'rock') ||
+      (playerSelection == 'scissors' && computerSelection == 'paper')){
+    return 'You win';
+  } else {
+    return 'You lose'
+  }
+  }
+
+function game() {
+  for (let index = 0; index < 5; index++) {
+    console.log(playRound());
+  }
+}
+
